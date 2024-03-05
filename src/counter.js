c9baeffb17261c9451333be0cab0274d24fe09bb
Manu S <ms.manu.856@gmail.com>
2024-03-05 14:38:56 +0530

    useEffect

--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 
 
 function Counter() {
   const [count, setCount] = useState(0)
+  useEffect(()=> {
+    console.log("Mounting...", count);
+    return ()=>{
+      console.log("Update..", count);
+    }
+  },[count])
   return (
     <>
     <button  onClick={() => setCount(count +  1)}>Increment</button>

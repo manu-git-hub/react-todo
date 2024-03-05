6de2755fa2974391dc779b77e0a129564d9ad944
Manu S <ms.manu.856@gmail.com>
2024-03-05 14:10:15 +0530

    mounting

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import { useState } from "react";
 import React from 'react';
-import Employee from "./Employee";
+import Counter from "./counter";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState(false);
 
-  const handleButtonClick = () => {    
-      setCount(count + 1);
-  };
-
-  let emp =[{name:"Manu", age:21},
-  {name:"Bob", age:22},
-  {name:"Steve", age:25},
-
-]
   return (
-    <div className='App'>
-      <button onClick={handleButtonClick}>Click me</button>
-      <h1>Hi</h1>
-      {
-        emp.map((obj,index) => 
-         (
-          // <Employee key={index} name={obj.name} age={obj.age}/>
-          <Employee key={index} {...obj}/>
-        )
-      )}
-    </div>    
+    <div className="app">
+      <h1 onClick={() => setState(!state) }>React Hooks Example</h1>
+      {/* Mounting & Unmounting */}
+      {/* {state ? <Counter /> : null} */}
+      {state && <Counter /> }
+    </div>
   );
 }
 
---
+++ src/counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Counter() {
+  return (
+    <div>Hello I am counter</div>
+  )
+}
+
+export default Counter
